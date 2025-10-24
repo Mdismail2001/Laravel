74983201f504220507be7523efcaadcfc2fae91f
md ismail <mdismail85773@gmail.com>
2025-10-24 18:36:27 +0800

    create a table for show all task

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,14 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task</title>
+    <title>Task Page</title>
 </head>
 <body>
     <h1>Task List</h1>
-    <ul>
-        @foreach ($tasks as $task)
-            <li>{{ $task->name}} - {{ $task->description }}</li>
-        @endforeach
-    </ul>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($tasks as $task)
+                <tr>
+                    <td>{{ $task->id }}</td>
+                    <td>{{ $task->name }}</td>
+                    <td>{{ $task->description }}</td>
+                    <td>
+                        <a href="">Edit</a>
+                        <form action="" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                        </form>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
